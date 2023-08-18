b57d6a40b27d3890456b2557e1e4faf816767920
hemalathasrianusuri <104627794+hemalathasrianusuri@users.noreply.github.com>
2023-08-18 22:58:30 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section"style="border-top: 5px solid white;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; <?php echo date('Y');?> Online Library Management System</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
